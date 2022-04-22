64fa7686fa07eabb6b0df2afabe2adbd7af62660
TecHeReTiC <talex73@mail.ru>
2022-04-22 16:28:41 +0500

    GUI for settings is in progress. gui's logic is in progress.

--- classes/guis.py
+++ classes/guis.py
@@ -1,26 +1,38 @@
 import PySimpleGUI as sg
+from scripts.game_manager import GameManager
 
 class Settings:
 
-    def __init__(self):
+    def __init__(self, manager: GameManager):
+        self.manager = manager
+
         sg.theme('DarkAmber')
+        sg.set_options(font='Frank 12')
         self.layout = [
             [sg.Frame('Game', [
                 [sg.Checkbox('Blood', default=True, text_color='red', key='-BLOOD-')]
             ])],
             [sg.HorizontalSeparator()],
             [sg.Frame('Graphics', [
-                [sg.Text('Resolution'), sg.Spin(['720x480', '900x600'])]
-            ])]
+                [sg.Text('Resolution'), sg.Spin(['720x480', '900x600'], key='-RES-'),
+                 sg.Checkbox('Fullscreen', key='-FULLSCREEN-')]
+            ],)],
+            [sg.Push(), sg.Button('Reset', button_color='red'), sg.Button('Apply', button_color='green')]
         ]
-        self.window = sg.Window('Settings', layout=self.layout)
+        self.window = sg.Window('Settings', layout=self.layout, element_justification='left')
 
     def run(self):
 
         while True:
-            event, key = self.window.read()
+            event, values = self.window.read()
 
-            if event == sg.WIN_CLOSED:
+            if event == sg.WIN_CLOSED or event == 'Reset':
+                self.close()
+                break
+
+            elif event == 'Apply':
+                blood, res, full = values['-BLOOD-'], values['-RES-'].split('x'), values['-FULLSCREEN-']
+                print(blood, res, full)
                 self.close()
                 break
 
@@ -28,6 +40,8 @@ class Settings:
         self.window.close()
         print('closed')
 
-settings = Settings()
+manager = GameManager()
+
+settings = Settings(manager)
 
 settings.run()
--- scripts/game_manager.py
+++ scripts/game_manager.py
@@ -1,3 +1,7 @@
+import pygame
+
+from scripts.constants_and_sources import display_width, display_height
+
 class GameManager:
     possible_states = ['main_menu',
                        'settings',
@@ -10,3 +14,17 @@ class GameManager:
         if state not in self.possible_states:
             raise RuntimeError('State is not supported')
         self.state = state
+        self.blood = True
+        self.res = (display_width, display_height)
+        self.full = False
+
+    def update(self, res: tuple, blood=True, full=False) -> pygame.Surface:
+        self.res = res
+        self.blood = blood
+        self.full = full
+
+        if self.full:
+            return pygame.display.set_mode(self.res, pygame.FULLSCREEN)
+        return pygame.display.set_mode(self.res)
+
+
